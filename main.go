3c9c41fc14b77b9e061bfc1146bf8d7c8d9c912f
Jake <77035482+JacobMonck@users.noreply.github.com>
2023-02-23 15:40:26 +0000

    feat: add base go project

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	err := godotenv.Load()
+	if err != nil {
+	  logrus.Fatal("Error loading .env file, please ensure you have created one in the root directory.")
+	}
+
+	logrus.Info("Initialization complete.")
+}
+
+func main() {}
